d0d15b92074e17bc32a0bbe80cf94da18e39c09e
d3oxy <ishansachu1@gmail.com>
2025-02-05 22:56:42 +0530

    feat: add viewport meta tag for responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,46 +6,37 @@ import { Gotu, Poppins, Playfair_Display } from "next/font/google";
 import Providers from "@/app/Providers";
 
 export const metadata: Metadata = {
-  title:
-    "Lara Restaurant Leicester | Authentic Turkish Restaurant | Grill Platters Famous",
-  description:
-    "Lara Restaurant in Leicester, London, offers authentic Turkish cuisine with mouthwatering grill platters, tender lamb dishes, and a variety of flavorful, traditional Turkish recipes. Experience a true taste of Turkey in a warm, welcoming atmosphere!",
-  icons: [{ rel: "icon", url: "/images/logo.png" }],
+    title: "Lara Restaurant Leicester | Authentic Turkish Restaurant | Grill Platters Famous",
+    description:
+        "Lara Restaurant in Leicester, London, offers authentic Turkish cuisine with mouthwatering grill platters, tender lamb dishes, and a variety of flavorful, traditional Turkish recipes. Experience a true taste of Turkey in a warm, welcoming atmosphere!",
+    icons: [{ rel: "icon", url: "/images/logo.png" }],
 };
 
 const manrope = Gotu({
-  subsets: ["latin"],
-  variable: "--font-gotu",
-  weight: ["400"],
+    subsets: ["latin"],
+    variable: "--font-gotu",
+    weight: ["400"],
 });
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  variable: "--font-poppins",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"],
+    variable: "--font-poppins",
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const playfair = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-playfair",
-  weight: ["400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"],
+    variable: "--font-playfair",
+    weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "theme-custom flex min-h-screen bg-[#070707] font-poppins text-[#FBEAD2] antialiased",
-          manrope.variable,
-          poppins.variable,
-          playfair.variable,
-        )}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+            <body className={cn("theme-custom flex min-h-screen bg-[#070707] font-poppins text-[#FBEAD2] antialiased", manrope.variable, poppins.variable, playfair.variable)}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
